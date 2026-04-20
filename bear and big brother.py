b7ce8263b9c9868834c117c4f7a62f574a1ccf2c
imran <imranidris10999@gmail.com>
2026-04-19 22:08:29 -0700

    Debugged a bug

--- bear and big brother.py
+++ bear and big brother.py
@@ -1,10 +1,8 @@
 #codeforces problem 791 A
-a = int(input())
-b = int(input())
+a, b = map(int, input().split())
 year = 0
 while( a <= b):
     a = a*3
     b = b*2
     year += 1
-    continue
 print(year)
