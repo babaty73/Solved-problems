04851c1b3d4798d527018416e769be4c77bb4f88
imran idris <imranidris10999@gmail.com>
2026-07-02 22:55:03 +0300

    Solved a problem

---
+++ Solider and banana.py
@@ -0,0 +1,8 @@
+#Codeforces problem - 546A Solider and banana
+k, n, w = map(int, input().split())
+total_cost = k * w * (w + 1) // 2
+borrow = total_cost - n
+if borrow > 0:
+    print(borrow)
+else:
+    print(0)
