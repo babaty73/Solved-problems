7e52e2932b3cc35eed5f7fb16c449c176d942f36
imran idris <imranidris10999@gmail.com>
2026-07-13 22:31:22 +0300

    Solved a problem

---
+++ Sequential digits.py
@@ -0,0 +1,16 @@
+#leetcode problem 1291 sequential digits 
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        result = []
+
+        for start in range(1, 10):
+            num = start
+
+            for next_digit in range(start + 1, 10):
+                num = num * 10 + next_digit
+
+                if low <= num <= high:
+                    result.append(num)
+
+        result.sort()
+        return result
